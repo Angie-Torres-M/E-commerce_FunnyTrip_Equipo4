d4aec9bc4938e14d6ab84dbd04fc89a4b1a5cdd1
emiliano <mcarlosemiliano5@gmail.com>
2025-11-28 09:09:32 -0600

    Se implementan los archivos        carrito.html         login.html         pages/otrocss/carrito.css         pages/otrocss/login.css         pages/otrocss/productos.css         productos.html         scripts/carrito.js         scripts/login.js         scripts/productos.js

---
+++ scripts/carrito.js
@@ -0,0 +1,11 @@
+// scripts/carrito.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("JS de carrito cargado");
+
+  // Aquí irá la lógica del carrito
+  // - Obtener carrito
+  // - Renderizar items
+  // - Eliminar items
+  // - Calcular total
+});
